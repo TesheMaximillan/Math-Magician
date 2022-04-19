5bed078917af8cdaad429532b2a85e19d61eca24
Teshome Kurabachew <dear.teshe@gmail.com>
2022-04-19 21:47:00 +0300

    Create class component

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,86 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+
+class Calculator extends React.Component {
+  render() {
+    return (
+      <table className="calculator">
+        <tr className="calculator__row">
+          <td className="calculator__column calculator__column--top" colSpan="4">
+            <input type="text" className="result" disabled value="0" />
+          </td>
+        </tr>
+        <tr className="calculator__row">
+          <td className="calculator__column calculator__column--main">
+            <input type="button" className="operator" value="AC" />
+          </td>
+          <td className="calculator__column calculator__column--main">
+            <input type="button" className="operator" value="+/-" />
+          </td>
+          <td className="calculator__column calculator__column--main">
+            <input type="button" className="operator" value="%" />
+          </td>
+          <td className="calculator__column calculator__column--side">
+            <input type="button" className="operator" value="÷" />
+          </td>
+        </tr>
+        <tr className="calculator__row">
+          <td className="calculator__column calculator__column--main">
+            <input type="button" className="number" value="7" />
+          </td>
+          <td className="calculator__column calculator__column--main">
+            <input type="button" className="number" value="8" />
+          </td>
+          <td className="calculator__column calculator__column--main">
+            <input type="button" className="number" value="9" />
+          </td>
+          <td className="calculator__column calculator__column--side">
+            <input type="button" className="operator" value="x" />
+          </td>
+        </tr>
+        <tr className="calculator__row">
+          <td className="calculator__column calculator__column--main">
+            <input type="button" className="number" value="4" />
+          </td>
+          <td className="calculator__column calculator__column--main">
+            <input type="button" className="number" value="5" />
+          </td>
+          <td className="calculator__column calculator__column--main">
+            <input type="button" className="number" value="6" />
+          </td>
+          <td className="calculator__column calculator__column--side">
+            <input type="button" className="operator" value="-" />
+          </td>
+        </tr>
+        <tr className="calculator__row">
+          <td className="calculator__column calculator__column--main">
+            <input type="button" className="number" value="1" />
+          </td>
+          <td className="calculator__column calculator__column--main">
+            <input type="button" className="number" value="2" />
+          </td>
+          <td className="calculator__column calculator__column--main">
+            <input type="button" className="number" value="3" />
+          </td>
+          <td className="calculator__column calculator__column--side">
+            <input type="button" className="operator" value="+" />
+          </td>
+        </tr>
+        <tr className="calculator__row">
+          <td className="calculator__column calculator__column--main" colSpan="2">
+            <input type="button" className="number" value="0" />
+          </td>
+          <td className="calculator__column calculator__column--main">
+            <input type="button" className="number" value="." />
+          </td>
+          <td className="calculator__column calculator__column--side">
+            <input type="button" className="operator" value="=" />
+          </td>
+        </tr>
+      </table>
+    );
+  }
+}
+
+export default Calculator;
